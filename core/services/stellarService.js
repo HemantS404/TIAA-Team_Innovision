d6b9bb6c6ecac8e270936d382795bd2b0d53e617
Hemant <hphero272@gmail.com>
2023-11-21 00:19:11 +0530

    Initail

---
+++ core/services/stellarService.js
@@ -0,0 +1,324 @@
+import StellarSdk, { FastSigning } from "stellar-sdk";
+import StorageService from './storageService';
+
+const DEFAULT_TX_TIMEOUT_SEC = 180;
+const TX_FEE = "1000"; //default is 100
+
+const server = new StellarSdk.Server("https://horizon-testnet.stellar.org");
+
+export const isAccountExists = async (account) => {
+  return new Promise((resolve, reject) => {
+    server
+      .loadAccount(account)
+      .catch(function (error) {
+        if (error instanceof StellarSdk.NotFoundError) {
+          resolve(false);
+        } else {
+          reject(error);
+        }
+      })
+      .then(function () {
+        resolve(true);
+      });
+  });
+};
+
+export const createKeyPair = () => {
+  // create a completely new and unique pair of keys
+  // see more about KeyPair objects: https://stellar.github.io/js-stellar-sdk/Keypair.html
+  const keypair = StellarSdk.Keypair.random();
+  return keypair;
+};
+
+export const getKeyPair = (secretKey) => {
+  try {
+    return StellarSdk.Keypair.fromSecret(secretKey);
+  } catch {
+    return null;
+  }
+};
+
+export const generateKeyAndCreateAccount = async () => {
+  return new Promise((resolve, reject) => {
+    const keypair = createKeyPair();
+    createAccount(keypair)
+      .then((result) => {
+        resolve(keypair.secret());
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+};
+
+export const createAccount = async (keypair) => {
+  try {
+    const response = await fetch(
+      `https://friendbot.stellar.org?addr=${encodeURIComponent(
+        keypair.publicKey()
+      )}`
+    );
+    const responseJSON = await response.json();
+    return true;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getBalance = async (accountPublicKey) => {
+  return new Promise((resolve, reject) => {
+    let result = [];
+    server
+      .loadAccount(accountPublicKey)
+      .then((account) => {
+        account.balances.forEach(function (balance) {
+          if (balance.asset_type === 'native') {
+            result.push({
+              assetType: 'XLM',
+              balance: balance.balance,
+            });
+          } else {
+            result.push({
+              assetType: balance.asset_type,
+              balance: balance.balance,
+            });
+          }
+        });
+        resolve(result);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+};
+
+export const getAssetByName = (assetName) => {
+  if (assetName.toLowerCase() == "native") {
+    return StellarSdk.Asset.native();
+  }
+  if (assetName.toLowerCase() == "usd") {
+    return new StellarSdk.Asset(
+      "USD",
+      "GDGU5OAPHNPU5UCLE5RDJHG7PXZFQYWKCFOEXSXNMR6KRQRI5T6XXCD7"
+    );
+  }
+
+  return StellarSdk.Asset.native();
+};
+
+export const makePayment = (
+  senderKeypair,
+  recipientAccount,
+  asset,
+  amount,
+  memo
+) => {
+  // Transaction will hold a built transaction we can resubmit if the result is unknown.
+  var transaction;
+
+  // Check to make sure that the destination account exists.
+  server
+    .loadAccount(recipientAccount)
+    .catch(function (error) {
+      if (error instanceof StellarSdk.NotFoundError) {
+        throw new Error("The recipient account does not exist.");
+      } else return error;
+    })
+    .then(function () {
+      return server.loadAccount(senderKeypair.publicKey());
+    })
+    .then(function (senderAccount) {
+      transaction = new StellarSdk.TransactionBuilder(senderAccount, {
+        fee: TX_FEE,
+        networkPassphrase: StellarSdk.Networks.TESTNET,
+      })
+        .addOperation(
+          StellarSdk.Operation.payment({
+            destination: recipientAccount,
+            asset: asset,
+            amount: amount,
+          })
+        )
+        .addMemo(StellarSdk.Memo.text(memo))
+        .setTimeout(DEFAULT_TX_TIMEOUT_SEC)
+        .build();
+
+      transaction.sign(senderKeypair);
+
+      return server.submitTransaction(transaction);
+    })
+    .then(function (result) {
+      console.log("Success! Results:", JSON.stringify(result));
+    })
+    .catch(function (error) {
+      console.error("Something went wrong!", JSON.stringify(error));
+      // If the result is unknown (no response body, timeout etc.) we simply resubmit
+      // already built transaction:
+      // server.submitTransaction(transaction);
+    });
+};
+
+export const getAccountTransactions = (account) => {
+
+  return new Promise((resolve, reject) => {
+    server.transactions()
+    .forAccount(account)
+    .call()
+    .then(function (result) {
+      console.log(`[getAccountTransactions]`);
+      console.log(result);
+      resolve(result);
+    })
+    .catch(function (err) {
+      console.error(err);
+      reject(err);
+    })
+  });
+};
+
+export const getLatestClaimableBalanceId = async (publicKey) => {
+  const res = await fetch(`https://horizon-testnet.stellar.org/claimable_balances?sponsor=${publicKey}&order=desc`);
+  const claimables = await res.json();
+
+  if (claimables) {
+    return claimables._embedded.records[0].id;
+  } else {
+    return null;
+  }
+}
+export const getClaimablesByCreator = async (publicKey) => {
+
+  // TODO: Note on local storage:
+  // The claimable_balances endpoint returns the list of claimable balances by sponsor
+  // However the memo text was not included in the response. So for demo purpose we are using local storage.
+  // The actual transaction is submitted and exists on the network.
+  
+  let result = StorageService.getClaimables();
+  return result.filter(x => x.sender === publicKey);
+};
+
+export const getClaimablesByBeneficiary = async (publicKey) => {
+
+  // TODO: Note on local storage:
+  // The claimable_balances endpoint returns the list of claimable balances by sponsor
+  // We haven't yet been able to retrieve by claimant destination. So for demo purpose we are using local storage.
+  // The actual transaction is submitted and exists on the network.
+
+  // server
+  //   .claimableBalances({ claimant: value })
+  //   .call()
+  //   .then(result => {
+  //     console.log(result);
+  //   });
+
+  let result = StorageService.getClaimables();
+  return result.filter(x => x.beneficiaryAccount === publicKey);
+};
+
+export const buildCreateClaimableBalanceTransaction = (
+    creatorKeypair,
+    beneficiaryAccount,
+    asset,
+    amount,
+    name,
+    claimableDate) => {
+
+  return new Promise((resolve, reject) => {
+    server
+      .loadAccount(beneficiaryAccount)
+      .catch(function (error) {
+        if (error instanceof StellarSdk.NotFoundError) {
+          reject("The beneficiary account does not exist.");
+        } else reject(error);
+      })
+      .then(function () {
+        return server.loadAccount(creatorKeypair.publicKey());
+      })
+      .then(function (creatorAccount) {
+        const creatorPredicate = StellarSdk.Claimant.predicateBeforeAbsoluteTime(
+          claimableDate.getTime().toString()
+        );
+        const inheritorPredicate = StellarSdk.Claimant.predicateNot(
+          creatorPredicate
+        );
+
+        const claimants = [
+          new StellarSdk.Claimant(creatorKeypair.publicKey(), creatorPredicate),
+          new StellarSdk.Claimant(beneficiaryAccount, inheritorPredicate),
+        ];
+
+        let transaction = new StellarSdk.TransactionBuilder(creatorAccount, {
+          fee: TX_FEE,
+          networkPassphrase: StellarSdk.Networks.TESTNET,
+        })
+          .addOperation(
+            StellarSdk.Operation.createClaimableBalance({
+              asset: asset,
+              amount: amount,
+              claimants: claimants,
+            })
+          )
+          .addMemo(StellarSdk.Memo.text(name))
+          .setTimeout(DEFAULT_TX_TIMEOUT_SEC)
+          .build();
+
+        transaction.sign(creatorKeypair);
+
+        resolve(transaction);
+      })
+      .catch(function (error) {
+        reject(error);
+      });
+  });
+}
+
+export const buildClaimTransaction = (beneficiaryKeypair, balanceId) => {
+  
+  return new Promise((resolve, reject) => {
+    server
+      .loadAccount(beneficiaryKeypair.publicKey())
+      .then(function (account) {
+
+        let transaction = new StellarSdk.TransactionBuilder(account, {
+          fee: TX_FEE,
+          networkPassphrase: StellarSdk.Networks.TESTNET,
+        })
+        .addOperation(
+          StellarSdk.Operation.claimClaimableBalance({
+            balanceId: balanceId,
+          })
+        )
+        .addMemo(StellarSdk.Memo.text("Claim balance"))
+        .setTimeout(DEFAULT_TX_TIMEOUT_SEC)
+        .build();
+
+      transaction.sign(beneficiaryKeypair);
+
+      resolve(transaction);
+    })
+    .catch(function (error) {
+      reject(error);
+    });
+  });
+}
+
+export const submitTransaction = (transaction) => {
+  
+  // // TODO: test
+  // return new Promise((resolve, reject) => {
+  //   setTimeout(function(){ 
+  //     resolve(true);
+  //   }, 3000);
+  // });
+
+  return new Promise((resolve, reject) => {
+    server.submitTransaction(transaction)
+    .then(result => {
+      resolve(true);
+    })
+    .catch(function (error) {
+      console.log(error);
+      resolve(false);
+    });
+  });
+};
---
+++ pages/login.js
@@ -0,0 +1,238 @@
+import Head from "next/head";
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import styles from "./../styles/pages/login.style.js";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import {
+  getKeyPair,
+  isAccountExists,
+  generateKeyAndCreateAccount,
+} from "./../core/services/stellarService";
+import StorageService from './../core/services/storageService';
+
+export default function Login() {
+  const router = useRouter();
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [showLoginByKey, setShowLoginByKey] = useState(false);
+  const [showCreateAccount, setShowCreateAccount] = useState(false);
+  const [input, setInput] = useState("");
+  const [newAccountSecret, setNewAccountSecret] = useState("");
+
+  const handleCloseLoginByKey = () => setShowLoginByKey(false);
+  const handleShowLoginByKey = () => setShowLoginByKey(true);
+  const handleCloseCreateAccount = () => setShowCreateAccount(false);
+  const handleShowCreateAccount = () => {
+    setShowCreateAccount(true);
+    setIsLoading(true);
+    generateKeyAndCreateAccount().then((newSecret) => {
+      setIsLoading(false);
+      setNewAccountSecret(newSecret);
+    });
+  };
+
+  const loginWithSecret = () => {
+    const keypair = getKeyPair(input);
+    if (!keypair) {
+      setInput("");
+      alert("The format of the key is incorrect.");
+      return;
+    }
+
+    setIsLoading(true);
+    isAccountExists(keypair.publicKey()).then((exists) => {
+      setIsLoading(false);
+      if (exists) {
+        StorageService.setLoggedInKey(input);
+        router.push("/account");
+      } else {
+        setInput("");
+        alert("Unable to login. Please ensure that account exists on testnet.");
+      }
+    });
+  };
+
+  return (
+    <div className="main-container">
+      <Head>
+        <title>Waris - Login</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+          integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2"
+          crossOrigin="anonymous"
+        ></link>
+        <script
+          src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+          integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+          crossOrigin="anonymous"
+        ></script>
+        <script
+          src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
+          integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx"
+          crossOrigin="anonymous"
+        ></script>
+        <script
+          src="https://kit.fontawesome.com/23a717da0a.js"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
+
+      <header id="header" className="header-fixed">
+        <div className="container">
+          <div id="logo" className="pull-left">
+            <h1>
+              <a href="#intro" className="scrollto">
+                Waris
+              </a>
+            </h1>
+          </div>
+          <nav id="nav-menu-container">
+            <ul className="nav-menu">
+              <li>
+                <a href="/">Back to Home</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+
+      <section id="more-features" className="section-bg">
+        <div className="container">
+          <div className="section-header">
+            <h3 className="section-title">Access Your Account</h3>
+            <span className="section-divider"></span>
+            <p className="section-description">
+              Your Waris account on the blockchain is linked to your Stellar
+              wallet. <br />
+              You access your private notes by logging in using your wallet
+              keyfile.
+            </p>
+          </div>
+
+          <div className="row">
+            <div className="col-lg-12">
+              <div className="file-input box">
+                <div style={{ padding: 40 + "px" }}>
+                  <div className="icon">
+                    <i className="fas fa-key icon-large"></i>
+                  </div>
+                  <h4 className="title">
+                    <a onClick={handleShowLoginByKey}>Login with Secret Key</a>
+                  </h4>
+                  <p className="description">
+                    Your secret key will only be kept on your device browser.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-lg-12">
+              <div className="file-input box">
+                <div style={{ padding: 40 + "px" }}>
+                  <div className="icon">
+                    <i className="fas fa-wallet icon-large"></i>
+                  </div>
+                  <h4 className="title">
+                    <a>Login with Wallet (Coming Soon)</a>
+                  </h4>
+                  <p className="description">Drop or browse your wallet here.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-lg-12 section-header">
+              <p className="description create-account">
+                <a className="create-account" onClick={handleShowCreateAccount}>
+                  Don't have a wallet? Create one for free.
+                </a>
+              </p>
+            </div>
+          </div>
+          <div className="row" style={{ marginTop: 20 + "px" }}>
+            <div className="col-lg-12 section-header">
+              <i className="fas fa-exclamation-triangle warning-icon"></i>
+              <p className="section-description" style={{ textAlign: "left" }}>
+                Alpha Build: Waris is currently in development and will use the
+                Stellar Testnet for account creation and all transactions.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <Modal show={showLoginByKey} onHide={handleCloseLoginByKey}>
+        <Modal.Header closeButton>
+          <Modal.Title>Login with Secret Key</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div>
+            <div className="form-group">
+              <input
+                className="form-control"
+                value={input}
+                onInput={(e) => setInput(e.target.value)}
+                placeholder="Your Stellar account secret key"
+              />
+              <small className="form-text text-muted">
+                We'll never store your secret key.
+              </small>
+            </div>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          {!isLoading && (
+            <>
+              <Button variant="secondary" onClick={handleCloseLoginByKey}>
+                Cancel
+              </Button>
+              <Button variant="primary" onClick={loginWithSecret}>
+                Login
+              </Button>
+            </>
+          )}
+          {isLoading && (
+            <div>
+              <Button variant="primary">Logging in...</Button>
+            </div>
+          )}
+        </Modal.Footer>
+      </Modal>
+
+      <Modal show={showCreateAccount} onHide={handleCloseCreateAccount}>
+        <Modal.Header closeButton>
+          <Modal.Title>Creating New Account</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {isLoading && <div>Creating your account. Please wait...</div>}
+          {!isLoading && newAccountSecret && (
+            <div>
+              <p>
+                Your account is created. Please take note of your secret key and
+                keep it safely.
+              </p>
+              <textarea disabled className="key-textarea">
+                {newAccountSecret}
+              </textarea>
+            </div>
+          )}
+        </Modal.Body>
+        <Modal.Footer>
+          {!isLoading && (
+            <Button variant="secondary" onClick={handleCloseCreateAccount}>
+              Close
+            </Button>
+          )}
+        </Modal.Footer>
+      </Modal>
+
+      <style jsx global>
+        {styles}
+      </style>
+    </div>
+  );
+}
